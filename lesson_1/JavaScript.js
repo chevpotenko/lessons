5d840bced11635411011f88a325cc8fd8d673e6f
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2016-01-30 10:52:36 +0200

    rework ol lessons 1,2

--- lesson_1/JavaScript.js
+++ lesson_1/JavaScript.js
@@ -1,11 +1,41 @@
-﻿/*LESSON 1 ---------------------------------------------------------*/
-var operand = prompt('Enter operand', 2);
-var degree = prompt('Enter degree of operand', 2);
+﻿(function () {
 
-function pow(operand, degree) {
-  var result = 1;
-  for (i = 0; degree > i; ++i) { result = result * operand; }
-  return result;
-}
-var example = pow(operand, degree);
-console.log(example);
+  var operand;
+  var degree;  
+  var controlSet = [-3, -2, -1, 0, 1, 2, 3];
+
+
+  function pow(operand, degree) {
+    var result = 1;
+    var absDegree = Math.abs(degree);
+    for (var i = 0; absDegree > i; ++i) { result = result * operand };
+    if (degree <= -1) result = 1 / result;
+    
+    /*Exceptions.*/
+    if (operand == 0 && degree <= 0) result = 'overflow';
+    if (operand == 0 && degree == 1) result = 0;
+    if (((Math.abs(operand) == 1) && degree == 0) || operand == 1) result = 1;   
+
+    return result;
+  }
+
+
+  function printTest(set) {
+    for (var i = 0; i < set.length; ++i) {     
+      console.log('*******************************************************************');
+      for (var j = 0; j < set.length; ++j) {    
+        console.log('operand=', set[i], '---degree=', set[j], '-----result=', pow(set[i], set[j]));
+      }     
+    }
+  }
+
+  
+  console.log('Prompt');
+  operand = prompt('Enter operand', 2);
+  degree = prompt('Enter degree of operand', 2);
+  console.log('operand=', operand, '---degree=', degree, '-----result=', pow(operand, degree));
+
+  console.log('array of testing')
+  printTest(controlSet); 
+
+})();
--- lesson_2/JavaScript.js
+++ lesson_2/JavaScript.js
@@ -1,29 +1,21 @@
-﻿/*LESSON 2 ---------------------------------------------------------*/
-var arr = new Array(5);
-arr[0] = 'Ivan';
-arr[1] = 'Oleg';
-arr[2] = 'Marat';
-arr[3] = 'Egor';
-arr[4] = 'Gleb';
+﻿(function () {
 
-function fillMassiv(listNames) {
-  for (var i = 0; i < 5; ++i) {
-    var newName = prompt('Fill up the array', listNames[i]);
-    listNames[i] = newName;
-    console.log(listNames[i]);
+  var arr = ['Ivan', 'Oleg', 'Marat', 'Egor', 'Gleb'];  
+  var listNames = [];
+
+  function fillMassiv(newList, defaultList) {
+    for (var i = 0; i < defaultList.length; ++i) {    
+      newList.push(prompt('Fill up the array', defaultList[i]));
+      console.log(newList[i]);
+    };    
   };
-};
 
-var checkName = function (listNames, findName) {
-  for (i = 0; i < listNames.length; ++i) {
-    if (findName == listNames[i]) {
-      alert(listNames[i] + ', you enter successfully');
-      return;
-    };
+  var checkName = function (list, userName) {  
+    if (list.indexOf(userName) != '-1') { alert(list[list.indexOf(userName)] + ', you enter successfully') } else { alert('Error') };
   };
-  alert('Error');
-};
 
-fillMassiv(arr);
-var user = prompt('Enter name', 'Ivan');
-checkName(arr, user);
+  fillMassiv(listNames, arr);
+  var user = prompt('Enter name', 'Ivan');
+  checkName(listNames, user);
+
+})();
