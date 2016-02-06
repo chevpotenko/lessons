4104f9547d751fadc74faa005cc225eb279590e5
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2016-02-06 12:46:45 +0200

    L2, v.2.0

--- lesson_2/script.js
+++ lesson_2/script.js
@@ -3,7 +3,7 @@
   var arr = ['Ivan', 'Oleg', 'Marat', 'Egor', 'Gleb'];  
   var listNames = [];
 
-  function fillMassiv(newList, defaultList) {
+  function fillArray(newList, defaultList) {
     for (var i = 0; i < defaultList.length; ++i) {    
       newList.push(prompt('Fill up the array', defaultList[i]));
       console.log(newList[i]);
@@ -14,7 +14,7 @@
     if (list.indexOf(userName) != '-1') { alert(list[list.indexOf(userName)] + ', you enter successfully') } else { alert('Error') };
   };
 
-  fillMassiv(listNames, arr);
+  fillArray(listNames, arr);
   var user = prompt('Enter name', 'Ivan');
   checkName(listNames, user);
 
