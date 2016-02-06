621f5d82d4d2d5d4c504a6078e925cdf26114046
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2016-02-06 14:55:04 +0200

    L7-8, v.2.0

---
+++ lesson_7_8/script.js
@@ -0,0 +1,21 @@
+﻿(function ($) {
+  $.fn.tabControl = function (tab, page, activeClass) {
+
+    var item = $(this).children(tab).children(' li');
+    var content = $(this).children(page);
+
+    item.on("click", function () {
+      item.removeClass(activeClass);
+      $(this).addClass(activeClass);
+      content.hide();
+      content.eq($(this).index()).show();
+    })
+  }
+})(jQuery)
+  
+
+$(function () {
+  $('.pages').tabControl('.tab', '.page', 'active');  
+  $('.tooltip').on('mouseover', function (e) { $(this).toggleClass('titleForm'); });
+  $('.tooltip').on('mouseout', function (e) { $(this).removeClass('titleForm') });
+})
