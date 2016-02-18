ebc789e45203c5f9a3a6e50923a291d79ecaa78f
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2016-02-18 22:25:04 +0200

    L19-20, v.1.1

--- lesson_19_20/gruntfile.js
+++ lesson_19_20/gruntfile.js
@@ -1,56 +1,56 @@
 ﻿module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
+	// Project configuration.
+	grunt.initConfig({
 		
 		concat: {
-      options: {
-        separator: ';' 
-      },
-      dist: {
-        src: ['src_js/*.js'],
-        dest: 'js/script.js'
-      }
-    },
-		
-		uglify:{			
-			dist:{
-				src: ['js/script.js'],
-        dest: 'js/script.js'
-			}			
+			options: {
+				separator: ';' 
+			},
+			dist: {
+				src: ['src_js/*.js'],
+				dest: 'js/script.js'
+			}
 		},
 		
-    sass: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: 'scss',
-          src: ['*.scss'],
-          dest: 'css',
-          ext: '.css'
-        }]
-      }
-    },
+		uglify:{
+			dist:{
+				src: ['js/script.js'],
+				dest: 'js/script.js'
+			}
+		},
 		
-    watch: {
-      sass: {
-        // We watch and compile sass files as normal but don't live reload here
-        files: ['scss/*.scss'],
-        tasks: ['sass']
-      }
-    }
-    
-  });
+		sass: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: 'scss',
+					src: ['*.scss'],
+					dest: 'css',
+					ext: '.css'
+				}]
+			}
+		},
+		
+		watch: {
+			sass: {
+				// We watch and compile sass files as normal but don't live reload here
+				files: ['scss/*.scss'],
+				tasks: ['sass']
+			}
+		}
+		
+	});
 
-  // Load the plugin that provides the "uglify" task.
-  
+	// Load the plugin that provides the "uglify" task.
+	
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task(s).
+	// Default task(s).
 
-  grunt.registerTask('default', ['concat','uglify','sass','watch']);
+	grunt.registerTask('default', ['concat','uglify','sass','watch']);
 
 };
