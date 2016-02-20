04593ae2e5f06aefbc143954cc210dfd7199bef7
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2016-02-20 23:10:48 +0200

    L15-16, v.1.0

---
+++ lesson_15_16/js/jquery.googleQuery.js
@@ -0,0 +1,67 @@
+﻿(function ($) {
+
+  $.fn.googleQuery = function (options) {
+    var textQuery;
+    var response;
+    var key = 'ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg';
+    var root = $(this);
+    var intervalNetwork;
+
+    root.find('form').on('focusin', function () { $(this).addClass(options.classInput) })
+    .on('focusout', function () { $(this).removeClass(options.classInput) });
+    root.find('[type=submit]').on('click', sendQuery);
+
+    function sendQuery(e) {
+      textQuery = root.find('[type=text]').val();
+      setQuery();
+      if ($(root.find('.' + options.boxResult).hasClass(options.classHide))) {
+        root.find('.' + options.boxForm).addClass(options.classReplace);
+        root.find('.' + options.boxMenu).removeClass(options.classHide);
+        root.find('.' + options.boxResult).removeClass(options.classHide)
+        root.find('[type=submit]').val('');
+      }
+      root.find('.' + options.boxResult).find('li').remove();
+      e.preventDefault();
+    }
+
+    function setQuery() {
+      response = $.ajax({
+        // beforeSend: checkNetwork,
+        url: 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0'
+        + '&key=' + key
+        + '&q=' + textQuery
+        + '&rsz=8&callback=GoogleCallback&context=?',
+        dataType: 'jsonp',
+        success: doAnswer,
+        // error: warning
+      })
+    }
+
+    function doAnswer() {
+      var arrResponse = response.responseJSON.results;
+      var html = root.find('#' + options.boxTemplate).html();
+      var content = tmpl(html, { data: arrResponse });
+      root.find('.' + options.boxResult + ' ul').append(content);
+      if (options.idAmountResults) { root.find('#' + options.idAmountResults).html(arrResponse.length) };
+      if (options.idTextQuery) { root.find('#' + options.idTextQuery).html(textQuery) };
+      // if (intervalNetwork) { clearInterval(intervalNetwork) };
+    };
+
+    function warning(XMLHttpRequest, textStatus, thrownError) {
+      alert("Communication with the server is lost!");
+      if (options.idAmountResults) { root.find('#' + options.idAmountResults).html('-') };
+      if (options.idTextQuery) { root.find('#' + options.idTextQuery).html('-') };
+    }
+
+    function checkNetwork(jqXHR, settings) {     
+      if (!navigator.onLine) {
+        intervalNetwork = setInterval(jqXHR.abort, 200);
+      }
+    }
+  }
+
+})(jQuery)
+  
+function GoogleCallback(func, data) {
+  window[func](data);
+};
---
+++ lesson_15_16/js/script.js
@@ -0,0 +1,71 @@
+﻿$(function () {
+  $('#googleQuery').googleQuery({
+    boxForm: 'formQuery',
+    boxMenu: 'menu',
+    boxResult: 'result',
+    boxTemplate: 'text_tmpl',
+    classInput: 'activeInput',
+    classReplace: 'formQuery--replace',
+    classHide: 'hide',
+    idAmountResults: 'amountResults',
+    idTextQuery: 'textQuery',
+  });
+
+  /*-----------------------------------------------------------------------------------*/
+
+  var human_data = {
+    name: 'Ivan',
+    age: '20',
+    gender: 'female',
+    height: '170',
+    weight: '60'
+  };
+
+  var worker1_data = {
+    job: 'manager',
+    salary: '$2000$'
+  };
+
+  var student1_data = {
+    placeOfStudy: 'Oxford',
+    scholarship: '$1000'
+  };
+
+  var student2_data = {
+    placeOfStudy: 'Cambridge',
+    scholarship: '$500'
+  };
+
+  function Human(options) {
+    this.name = options.name;
+    this.age = options.age;
+    this.gender = options.gender;
+    this.height = options.height;
+    this.weight = options.weight;
+  };
+  var human = new Human(human_data);
+  console.log(human);
+
+
+  function Worker(options) {
+    this.job = options.job;
+    this.salary = options.salary;
+    this.work = function () { alert('working!') };
+  };
+  Worker.prototype = human;
+  var worker1 = new Worker(worker1_data);
+  console.log(worker1);
+
+
+  function Student(options) {
+    this.placeOfStudy = options.placeOfStudy;
+    this.scholarship = options.scholarship;
+    this.watchigSoapOperas = function () { alert('watching!') }
+  }
+  Student.prototype = human;
+  var student1 = new Student(student1_data);
+  console.log(student1);
+  var student2 = new Student(student2_data);
+  console.log(student2);
+
+})
