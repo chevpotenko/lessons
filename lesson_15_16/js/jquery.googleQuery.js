e4516df4af9312e97d98353e3f9be5c60255150c
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2016-02-28 15:47:01 +0200

    L15-16, v.1.1 (Ajax, prototype)

--- lesson_15_16/js/jquery.googleQuery.js
+++ lesson_15_16/js/jquery.googleQuery.js
@@ -1,67 +1,48 @@
 ﻿(function ($) {
 
-  $.fn.googleQuery = function (options) {
-    var textQuery;
-    var response;
-    var key = 'ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg';
-    var root = $(this);
-    var intervalNetwork;
+	$.fn.googleQuery = function (options) {
+		var textQuery;
+		var key = 'ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg';
+		var root = $(this);
 
-    root.find('form').on('focusin', function () { $(this).addClass(options.classInput) })
-    .on('focusout', function () { $(this).removeClass(options.classInput) });
-    root.find('[type=submit]').on('click', sendQuery);
+		function newQuery(e) {
+			e.preventDefault();
+			textQuery = root.find('[type=text]').val();
+			sendQuery();
+			if ($(root.find('.' + options.boxResult).hasClass(options.classHide))) {
+				root.find('.' + options.boxForm).addClass(options.classReplace);
+				root.find('.' + options.boxMenu).removeClass(options.classHide);
+				root.find('.' + options.boxResult).removeClass(options.classHide)
+				root.find('[type=submit]').val('');
+			}
+		}
 
-    function sendQuery(e) {
-      textQuery = root.find('[type=text]').val();
-      setQuery();
-      if ($(root.find('.' + options.boxResult).hasClass(options.classHide))) {
-        root.find('.' + options.boxForm).addClass(options.classReplace);
-        root.find('.' + options.boxMenu).removeClass(options.classHide);
-        root.find('.' + options.boxResult).removeClass(options.classHide)
-        root.find('[type=submit]').val('');
-      }
-      root.find('.' + options.boxResult).find('li').remove();
-      e.preventDefault();
-    }
+		function sendQuery() {
+			$.ajax({
+				url: 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0'
+				+ '&key=' + key
+				+ '&q=' + textQuery
+				+ '&rsz=8&callback=GoogleCallback&context=?',
+				dataType: 'jsonp',
+				success: doAnswer
+			})
+		}
 
-    function setQuery() {
-      response = $.ajax({
-        // beforeSend: checkNetwork,
-        url: 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0'
-        + '&key=' + key
-        + '&q=' + textQuery
-        + '&rsz=8&callback=GoogleCallback&context=?',
-        dataType: 'jsonp',
-        success: doAnswer,
-        // error: warning
-      })
-    }
+		function doAnswer(data) {
+			var html = root.find('#' + options.boxTemplate).html();
+			var content = tmpl(html, { data: data.results });
+			root.find('.' + options.boxResult + ' ul').html(content);
+			if (options.idAmountResults) { root.find('#' + options.idAmountResults).html(data.results.length) };
+			if (options.idTextQuery) { root.find('#' + options.idTextQuery).html(textQuery) };
+		};
 
-    function doAnswer() {
-      var arrResponse = response.responseJSON.results;
-      var html = root.find('#' + options.boxTemplate).html();
-      var content = tmpl(html, { data: arrResponse });
-      root.find('.' + options.boxResult + ' ul').append(content);
-      if (options.idAmountResults) { root.find('#' + options.idAmountResults).html(arrResponse.length) };
-      if (options.idTextQuery) { root.find('#' + options.idTextQuery).html(textQuery) };
-      // if (intervalNetwork) { clearInterval(intervalNetwork) };
-    };
-
-    function warning(XMLHttpRequest, textStatus, thrownError) {
-      alert("Communication with the server is lost!");
-      if (options.idAmountResults) { root.find('#' + options.idAmountResults).html('-') };
-      if (options.idTextQuery) { root.find('#' + options.idTextQuery).html('-') };
-    }
-
-    function checkNetwork(jqXHR, settings) {     
-      if (!navigator.onLine) {
-        intervalNetwork = setInterval(jqXHR.abort, 200);
-      }
-    }
-  }
+		root.find('form').on('focusin', function () { $(this).addClass(options.classInput) })
+		.on('focusout', function () { $(this).removeClass(options.classInput) });
+		root.find('[type=submit]').on('click', newQuery);
+	}
 
 })(jQuery)
-  
+
 function GoogleCallback(func, data) {
-  window[func](data);
+	window[func](data);
 };
