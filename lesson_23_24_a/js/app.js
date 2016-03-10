23715f60a1264194e8b5ca992cd5e88fa38de5d0
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2016-03-10 11:37:37 +0200

    L23-24, v.1.1. (AngularJS)

--- lesson_23_24_a/js/app.js
+++ lesson_23_24_a/js/app.js
@@ -1,65 +1,49 @@
 var task = angular.module('task',[]);
 
 task.controller('taskController', ['$scope', function($scope){
+
+	var firstMode='add';
+	var secondMode='edit';
+
 	$scope.model = [
 		{title:'HTML'},
 		{title:'CSS'},
 		{title:'JS'}
 	];
 	$scope.itemTitle='';
-	$scope.editAt=0;
-	$scope.editState=false;
+	$scope.editAt = 0;
+	$scope.btnText = firstMode;
 
 	$scope.edit = function($event){
 		if($scope.form.$invalid){
 			return
 		}
 		else{
-			if(!$scope.editState){
+			if($scope.btnText == firstMode){
 				$scope.model.push({title: $scope.itemTitle});
 			}
 			else{
 				$scope.model[$scope.editAt].title=$scope.itemTitle;
+				$scope.btnText = firstMode
 			}
 			$scope.itemTitle = '';
 		}
 	};
 
 	$scope.delete = function(index){
-		$scope.model.splice(index,1);
+		$scope.model.splice(index, 1);
 		$scope.itemTitle = '';
 	};
 
 	$scope.select = function(index){
 		$scope.editAt=index;
-		$scope.editState=true;
 		$scope.itemTitle = $scope.model[index].title;
+		$scope.btnText = secondMode;
 	};
-}]);
 
-task.directive('editTask', function(){
-	return {
-		link: function ($scope, $elem, $attrs){
-
-			$scope.$watch('editState', function(){
-				if($scope.editState){
-					$elem.addClass('btn--edit').text('edit');
-				}
-			});
-
-			$scope.$watchCollection('model', function(){
-				if($scope.editState){
-					$elem.removeClass('btn--edit').text('add');
-					$scope.editState=false;
-				}
-			});
-
-			$scope.$watch('itemTitle', function(){
-				if(!$scope.itemTitle && $scope.editState){
-					$elem.removeClass('btn--edit').text('add');
-					$scope.editState=false;
-				}
-			});
+	$scope.$watch('itemTitle', function(){
+		if(!$scope.itemTitle && ($scope.btnText == secondMode)){
+			$scope.btnText = firstMode;
 		}
-	}
-})
+	});
+}]);
